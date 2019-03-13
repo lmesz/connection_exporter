e455c15bdf2a7b9513a22683c078add7447daee1
Laszlo Meszaros <lacienator@gmail.com>
2019-03-13 12:59:40 +0100

    do not collect all the ports only the choosen ones

--- main.go
+++ main.go
@@ -14,15 +14,28 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 var (
 	//tcp        0      0 127.0.0.1:44076         127.0.0.1:1445          TIME_WAIT   -
-	pattern     = regexp.MustCompile(`(tcp|tcp6)\s+\d+\s+\d+\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+(\w+)`)
-	defaultPort = "9911"
-	desc        = fmt.Sprintf("Port on which exporter should listen. Default: %s", defaultPort)
-	port        = kingpin.Flag(
-		"config.port",
+	pattern      = regexp.MustCompile(`(tcp|tcp6)\s+\d+\s+\d+\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+(\w+)`)
+	defaultPort  = "9911"
+	desc         = fmt.Sprintf("Port on which exporter should listen. Default: %s", defaultPort)
+	portToListen = kingpin.Flag(
+		"config.portToListen",
 		desc,
 	).Default(defaultPort).String()
+	portsToWatch = kingpin.Flag(
+		"config.portsToWatch",
+		desc,
+	).Default("3306,11211,7000,9160,8080,9911").String()
 )
 
 type connectionCollector struct {
@@ -49,23 +62,25 @@ func (collector *connectionCollector) Collect(ch chan<- prometheus.Metric) {
 		log.Fatal(err)
 	}
 
-	splitted_result := strings.Split(string(out), "\n")
+	splittedResult := strings.Split(string(out), "\n")
 
-	for i := range splitted_result {
+	for i := range splittedResult {
 
-		line := splitted_result[i]
+		line := splittedResult[i]
 		if pattern.MatchString(line) {
 			f := pattern.FindStringSubmatch(line)
-			if _, ok := result[f[3]]; ok {
-				if _, ok := result[f[3]][f[4]]; ok {
-					result[f[3]][f[4]] = result[f[3]][f[4]] + 1
+			if stringInSlice(f[3], strings.Split(*portsToWatch, ",")) {
+				if _, ok := result[f[3]]; ok {
+					if _, ok := result[f[3]][f[4]]; ok {
+						result[f[3]][f[4]] = result[f[3]][f[4]] + 1
+					} else {
+						result[f[3]] = map[string]float64{}
+						result[f[3]][f[4]] = 1
+					}
 				} else {
 					result[f[3]] = map[string]float64{}
 					result[f[3]][f[4]] = 1
 				}
-			} else {
-				result[f[3]] = map[string]float64{}
-				result[f[3]][f[4]] = 1
 			}
 		}
 
@@ -83,7 +98,7 @@ func main() {
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
 
-	listenPort := fmt.Sprintf(":%s", *port)
+	listenPort := fmt.Sprintf(":%s", *portToListen)
 
 	prometheus.MustRegister(newConnectionCollector())
 	http.Handle("/metrics", promhttp.Handler())
