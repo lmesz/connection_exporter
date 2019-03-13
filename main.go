17325fb8bc9993127f5c9208d5a4021bb5bc8eb8
Laszlo Meszaros <lacienator@gmail.com>
2019-03-13 11:07:13 +0100

    generate buckets based on netstat result

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-  "bufio"
-  "net/http"
-  //"os/exec"
-  "os"
-  "regexp"
+	"net/http"
+	"os/exec"
+	"regexp"
+	"strings"
 
-  log "github.com/Sirupsen/logrus"
-  "github.com/prometheus/client_golang/prometheus/promhttp"
-  "github.com/prometheus/client_golang/prometheus"
+	log "github.com/Sirupsen/logrus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-  //tcp        0      0 127.0.0.1:44076         127.0.0.1:1445          TIME_WAIT   -
- pattern = regexp.MustCompile(`(tcp|tcp6)\s+\d+\s+\d+\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+(\w+)`)
+	//tcp        0      0 127.0.0.1:44076         127.0.0.1:1445          TIME_WAIT   -
+	pattern = regexp.MustCompile(`(tcp|tcp6)\s+\d+\s+\d+\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+\d+.\d+.\d+.\d+.\d+:(\d+)\s+(\w+)`)
 )
 
 type connectionCollector struct {
@@ -35,38 +34,34 @@ func (collector *connectionCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (collector *connectionCollector) Collect(ch chan<- prometheus.Metric) {
-  var result = make(map[string]map[string]float64)
-  /*
-  out, err := exec.Command("netstat -plantu").Output()
+	var result = make(map[string]map[string]float64)
+	out, err := exec.Command("netstat", "-plantu").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-  */
-  file, err := os.Open("test_data")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer file.Close()
 
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    line := scanner.Text()
+	splitted_result := strings.Split(string(out), "\n")
 
-    if (pattern.MatchString(line)) {
-      f := pattern.FindStringSubmatch(line)
-      if _, ok := result[f[3]]; ok {
-        if _, ok := result[f[3]][f[4]]; ok {
-          result[f[3]][f[4]] = result[f[3]][f[4]] + 1
-        } else {
-          result[f[3]] = map[string]float64{}
-          result[f[3]][f[4]] = 1
-        }
-      } else {
-        result[f[3]] = map[string]float64{}
-        result[f[3]][f[4]] = 1
-      }
-    }
-  }
+	for i := range splitted_result {
+
+		line := splitted_result[i]
+		if pattern.MatchString(line) {
+			f := pattern.FindStringSubmatch(line)
+			if _, ok := result[f[3]]; ok {
+				if _, ok := result[f[3]][f[4]]; ok {
+					result[f[3]][f[4]] = result[f[3]][f[4]] + 1
+				} else {
+					result[f[3]] = map[string]float64{}
+					result[f[3]][f[4]] = 1
+				}
+			} else {
+				result[f[3]] = map[string]float64{}
+				result[f[3]][f[4]] = 1
+			}
+		}
+
+	}
+
 	for source := range result {
 		for state := range result[source] {
 			ch <- prometheus.MustNewConstMetric(collector.connectionMetric, prometheus.CounterValue, result[source][state], source, state)
@@ -75,8 +70,8 @@ func (collector *connectionCollector) Collect(ch chan<- prometheus.Metric) {
 }
 
 func main() {
-  prometheus.MustRegister(newConnectionCollector())
-  http.Handle("/metrics", promhttp.Handler())
-  log.Info("Beginning to serve on port :8080")
-  log.Fatal(http.ListenAndServe(":8080", nil))
+	prometheus.MustRegister(newConnectionCollector())
+	http.Handle("/metrics", promhttp.Handler())
+	log.Info("Beginning to serve on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
